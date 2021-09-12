c4aa7b48058650e1be27e52e1de16d06ddf9009b
Daniel Lazar <lazardani0112@gmail.com>
2021-09-12 19:52:16 +0200

    Added setup script.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-touch src/app/config.ts
+# echo -e "export const apiKey = 'SECRET';" > src/app/config.ts
 
-echo -e "export const apiKey = '';" > src/app/config.ts
+config=src/app/config.ts
+
+generate() {
+	touch $config
+	read -p "Access key to API: " ACCESS_KEY
+	echo -e "export const apiKey = '$ACCESS_KEY';" > $config
+	echo "Done."
+}
+
+if [ -f "$config" ]; then
+	echo "The config file is existing."
+	read -p "Would you like to regenerate? (y/n) " answer
+	if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+		generate
+	fi
+else 
+	echo "Would you like to generate the config file? (y/n) "
+	if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+		generate
+	fi
+fi
